7c204339b487cc85e758a0761a13a4cb3203c91e
Daria-Misko <111460998+Daria-Misko@users.noreply.github.com>
2023-04-18 13:32:09 +0000

    Deploying to gh-pages from @ Daria-Misko/goit-react-hw-05-movies@35fdb8b8aa153a0f4361e24f3c13afc62375c3a6 🚀

---
+++ static/js/186.2d4999d5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[186],{186:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Hello 2"})}}}]);
+//# sourceMappingURL=186.2d4999d5.chunk.js.map
